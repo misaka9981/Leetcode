7a752df5fcd08203b27d216f394fb7bebce4a2a5
misaka9981 <932353913@qq.com>
2021-09-18 16:03:07 +0800

    292 easy

---
+++ 292.nim-游戏.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=java
+ *
+ * [292] Nim 游戏
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n < 4){
+            return true;
+        }else if(n == 4){
+            return false;
+        }
+        return canWinNim(n-4);
+    }
+}
+// @lc code=end
+
